0dfb22297f23fbc9b6d9378c24c42556cf0e8e79
Eguz614 <ericguz614@gmail.com>
2023-01-07 18:57:25 -0800

    merged my working folder with repo folder to prevent committing too much.

--- Develop/script.js
+++ Develop/script.js
@@ -1,49 +1,68 @@
-// Assignment Code
+// Variable Bank 
 var generateBtn = document.querySelector("#generate");
 var pwLength = 8;
 var pwArr = []
-var lower = [ 'a','b','c','d','e']
-var upper = ['A','B','C','D','E']
-var spec = ['!','@','#','$' ]
-var num = [1,2,3,4,5]
-//Defining generatePassword Function/
+var lower = [ 'a','b','c','d','e'];
+var upper = ['A','B','C','D','E'];
+var spec = ['!','@','#','$' ];
+var num = ['1','2','3','4','5','6','7','8','9','0'];
+
+// This function generates the password based on the size of "pwArr" after the options are confirmed from npcdialog function.
+
 function generatePassword(){
- 
-    npcdialog();
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-  
-    passwordText.value = password;
-  
-
-
+   
+  var password = '';
+  for( var i = 0; i < pwLength; i++){
+// This variable is using a math.floor and math.random to randomly generate a number based of the pwArr length and making sure the number doesn't have a decimal.
+    var rando = Math.floor(Math.random() * pwArr.length)   
+    password = password + pwArr[rando];
+  }
+  return password;
 }
 
-// Write password to the #password input
+// this function takes all the info from both the generatepassword() and npcdialog() to paste a password in the <textarea> on the html.
   function writePassword() {
-  
-
+     npcdialog(); 
+    var randorino = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = randorino
+    
 }
  
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Function that ask for parameters for password
 
 function npcdialog(){
+ 
+  // Empty array that will act as the value bank for the password generator.
 
+  pwArr = [];
+ 
+  // Sets the number of characters for the password, if the input isn't a number it does nothing. 
   
   pwLength = prompt(" length of password ? (8 - 128)");
+  
+  // These confirms will merge their content with the empty array "pwArr" when the user clicks ok .
+  
+  if (confirm( "Are lowercase ok?")) {      
+    pwArr = pwArr.concat(lower);
+  } 
+  if (confirm( "Are uppercase ok?")) {     
+    pwArr = pwArr.concat(upper);
+  } 
+  if (confirm( "Are special characters ok?")) {  
+    pwArr = pwArr.concat(spec);
+  } 
+  if (confirm( "Are numbers ok?")) {
+    pwArr = pwArr.concat(num);
+  } 
+   
+  // After all parameters for the password are checked the finally value bank will be returned .
+  
+  return pwArr;    
+  }
+  
 
-if (confirm( "Are lowercase ok?")) {
-  pwArr = pwArr.concat(lower);
-} 
-if (confirm( "Are uppercase ok?")) {
-  pwArr = pwArr.concat(upper);
-} 
-if (confirm( "Are special characters ok?")) {
-  pwArr = pwArr.concat(spec);
-} 
-if (confirm( "Are numbers ok?")) {
-  pwArr = pwArr.concat(num);
-} 
+// On button click , this event listener will run write password function.
 
-}
+generateBtn.addEventListener("click", writePassword);
+
